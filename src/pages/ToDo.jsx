32443127b63eeda253176f7b08bbe3574bf69eaa
KimByeongHwa <iambyeonghwa@gmail.com>
2023-06-08 14:12:57 +0900

    [Feat} To Do List 수정 UI 완성 및 Button 컴포넌트 css 수정

--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import StyledButton from '../components/StyledButton';
-import { getToDos, createToDo } from '../api/todo';
+import { getToDos, createToDo, updateToDo, deleteToDo } from '../api/todo';
 
 function ToDo() {
+    // 로그인 체크
     const isLogin = localStorage.getItem('token');
     const navigate = useNavigate();
 
@@ -12,9 +13,31 @@ function ToDo() {
         if (!isLogin) navigate('/signin');
     }, [isLogin]);
 
-    const [addInput, setAddInput] = useState('');
-    const [toDoList, setToDoList] = useState([]);
+    const [addInput, setAddInput] = useState(''); // 사용자가 input에 입력한 text를 담는다.
+    const [toDoList, setToDoList] = useState([]); // To Do List를 담는다.
+    const [isDisabledAdd, setIsDisAbledAdd] = useState(true); // 추가 input 공백 검사를 위해 사용
+    const [isUpdateMode, setIsUpdateMode] = useState(false); // 수정 모드 on/off
+    const [updateInput, setUpdateInput] = useState(''); // 수정 Input
+    const [isDisabledUpdate, setIsDisabledUpdate] = useState(false); // 수정 input 공백 검사를 위해 사용
 
+    function handleAddInput(e) {
+        setAddInput(e.target.value);
+        // console.log(addInput);
+    }
+
+    // input 공백 검사
+    function checkInputValidation() {
+        const addInputArr = addInput.split('');
+
+        if (addInputArr.length > 0) setIsDisAbledAdd(false);
+        else setIsDisAbledAdd(true);
+    }
+
+    useEffect(() => {
+        checkInputValidation();
+    }, [addInput]);
+
+    // To Do List 조회
     async function getToDoList() {
         const list = await getToDos();
         // console.log(list);
@@ -25,17 +48,30 @@ function ToDo() {
         getToDoList();
     }, []);
 
-    function handleAddInput(e) {
-        setAddInput(e.target.value);
-        // console.log(addInput);
+    // To Do List 추가
+    async function addToDoList() {
+        if (isDisabledAdd === false) {
+            const addResult = await createToDo(addInput);
+            setToDoList([...toDoList, addResult]);
+            setAddInput('');
+            // console.log('현재 리스트', toDoList);
+        } else alert('추가할 할 일을 입력해주세요.');
     }
 
-    async function addToDoList() {
-        const addResult = await createToDo(addInput);
-        setToDoList([...toDoList, addResult]);
-        setAddInput('');
-        // console.log('현재 리스트', toDoList);
+    // To Do List 수정
+    async function updateToDoList(todo, isCompleted) {
+        setIsUpdateMode(true);
+        console.log(todo, isCompleted);
+        // const updateResult = updateToDo(todo, isCompleted);
+        // console.log(updateResult);
     }
+
+    function toggleUpdate() {
+        setIsUpdateMode((prev) => !prev);
+    }
+
+    // To Do List 삭제
+    async function deleteToDoList() {}
 
     function handleEnter(e) {
         if (e.key === 'Enter') {
@@ -53,13 +89,17 @@ function ToDo() {
         <Container>
             <Title>To Do List</Title>
             <InputContainer>
-                <StyledInput
+                <AddInput
                     data-testid='new-todo-input'
                     onChange={handleAddInput}
                     onKeyDown={handleEnter}
                     value={addInput}
                 />
-                <StyledButton data-testid='new-todo-add-button' onClick={addToDoList}>
+                <StyledButton
+                    data-testid='new-todo-add-button'
+                    onClick={addToDoList}
+                    disabled={isDisabledAdd}
+                >
                     추가
                 </StyledButton>
             </InputContainer>
@@ -67,31 +107,47 @@ function ToDo() {
                 {toDoList.map((e) => {
                     return (
                         <ListLi key={e.id} isCompleted={e.isCompleted} userId={e.userId}>
-                            <label>
+                            <StyledLabel>
                                 <StyledCheckBox type='checkbox' />
-                                <TextToDo>{e.todo}</TextToDo>
-                                <StyledButton data-testid='modify-button' small>
-                                    수정
-                                </StyledButton>
-                                <StyledButton data-testid='delete-button' small>
-                                    삭제
-                                </StyledButton>
-                            </label>
+                                <ContentWrapper>
+                                    {isUpdateMode ? (
+                                        <UpdateInput data-testid='modify-input' />
+                                    ) : (
+                                        <TextToDo>{e.todo}</TextToDo>
+                                    )}
+                                </ContentWrapper>
+
+                                {isUpdateMode ? (
+                                    <Buttons>
+                                        <StyledButton data-testid='submit-button' small>
+                                            제출
+                                        </StyledButton>
+                                        <StyledButton
+                                            data-testid='cancel-button'
+                                            small
+                                            onClick={toggleUpdate}
+                                        >
+                                            취소
+                                        </StyledButton>
+                                    </Buttons>
+                                ) : (
+                                    <Buttons>
+                                        <StyledButton
+                                            data-testid='modify-button'
+                                            small
+                                            onClick={toggleUpdate}
+                                        >
+                                            수정
+                                        </StyledButton>
+                                        <StyledButton data-testid='delete-button' small>
+                                            삭제
+                                        </StyledButton>
+                                    </Buttons>
+                                )}
+                            </StyledLabel>
                         </ListLi>
                     );
                 })}
-                {/* <List>
-                    <label>
-                        <StyledCheckBox type='checkbox' />
-                        <span>TODO 1</span>
-                        <StyledButton data-testid='modify-button' small>
-                            수정
-                        </StyledButton>
-                        <StyledButton data-testid='delete-button' small>
-                            삭제
-                        </StyledButton>
-                    </label>
-                </List> */}
             </ListContainer>
         </Container>
     );
@@ -110,15 +166,16 @@ const Title = styled.div`
 `;
 
 const InputContainer = styled.div`
-    display: felex;
+    display: flex;
     align-items: center;
     justify-content: center;
 `;
 
-const StyledInput = styled.input`
+const AddInput = styled.input`
     width: 16rem;
-    padding: 0.6rem;
-    font-size: 1rem;
+    padding: 0 0.2rem;
+    font-size: 1.4rem;
+    font-weight: 500;
 `;
 
 const ListContainer = styled.div`
@@ -129,25 +186,51 @@ const ListContainer = styled.div`
 `;
 
 const ListLi = styled.li`
-    width: 100%;
     margin: 0 auto;
     font-size: 1.4rem;
     font-weight: 500;
     list-style: none;
+    margin-bottom: 1.4rem;
 
-    & + & {
-        /* margin-bottom: 1rem; */
+    :last-child {
+        margin-bottom: 0;
     }
 `;
 
+const StyledLabel = styled.label`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 const StyledCheckBox = styled.input`
-    transform: scale(1.5);
+    transform: scale(1.8);
     margin-right: 2rem;
+`;
+
+const ContentWrapper = styled.div`
+    width: 55%;
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const TextToDo = styled.div`
     display: inline-block;
-    width: 65%;
+    width: 100%;
+`;
+
+const UpdateInput = styled.input`
+    display: inline-block;
+    width: 92%;
+    height: 1.4rem;
+    font-size: 1.2rem;
+    font-weight: 500;
+    padding: 0 0.4rem;
 `;
 
 export default ToDo;
